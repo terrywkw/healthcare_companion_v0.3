bf0485c497937253538887e54112af3b7ccf6323
Terry Wang <terry.wang@taiwanredsoul.com>
2024-10-26 11:23:03 +0800

    Updates

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvancedHealthHub from './components/AdvancedHealthHub';
+
+function App() {
+  return (
+    <div className="h-screen">
+      <AdvancedHealthHub />
+    </div>
+  );
+}
+
+export default App;
